438a7fdc0841b3bbe84ca6ec3ea0c491ce59d231
Cyno India <cyno@Cynos-MacBook-Air.local>
2022-08-12 12:29:46 +0530

    first commit

--- sales/settings.py
+++ sales/settings.py
@@ -186,6 +186,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -199,3 +200,4 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
+
